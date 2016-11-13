c5a44ffd105863b6c235834eb321a917a159c796
Rachel <rachel.l.pavilanis@gmail.com>
2016-11-13 12:17:03 -0800

    added model and controller for Projects

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,18 @@
 Rails.application.routes.draw do
+  get 'projects/index'
+
+  get 'projects/new'
+
+  get 'projects/create'
+
+  get 'projects/edit'
+
+  get 'projects/update'
+
+  get 'projects/show'
+
+  get 'projects/destroy'
+
   root to: 'welcome#index'
 
   resources :posts
