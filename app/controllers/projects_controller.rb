bcffbdaecf68af647c49283ca6fde7a83340b71b
Rachel <rachel.l.pavilanis@gmail.com>
2016-11-13 12:46:01 -0800

    set up and styled basic project functionality

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,22 +1,51 @@
 class ProjectsController < ApplicationController
+  before_action :find_project, only: [:show, :edit, :update, :destroy]
   def index
+    @projects = Project.all.order("created_at desc").paginate(page: params[:page], per_page: 10)
   end
 
   def new
+    @project = Project.new
   end
 
   def create
+    @project = Project.new(project_params)
+
+    if @project.save
+      redirect_to @project, notice: "Ohh yeah...good job finishing a project and projecting it."
+    else
+      render 'new', notice: "Oh, no, I was unable to save this project."
+    end
   end
 
   def edit
   end
 
-  def update
-  end
-
   def show
   end
 
-  def destroy
+  def update
+    if @project.update(project_params)
+      redirect_to @project, notice: "Your project was successfully saved!"
+    else
+      render 'edit'
+    end
   end
+
+  def destroy
+    @project.destroy
+    redirect_to projects_path
+  end
+
+
+private
+
+  def project_params
+    params.require(:project).permit(:title, :description, :link, :slug)
+  end
+
+  def find_project
+    @project = Project.friendly.find(params[:id])
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,9 @@
 Rails.application.routes.draw do
-  get 'projects/index'
-
-  get 'projects/new'
-
-  get 'projects/create'
-
-  get 'projects/edit'
-
-  get 'projects/update'
-
-  get 'projects/show'
-
-  get 'projects/destroy'
 
   root to: 'welcome#index'
 
   resources :posts
+  resources :projects
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
