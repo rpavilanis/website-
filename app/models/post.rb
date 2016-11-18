61a1d861201ab3933ee0e41bbe3072fd95ed539f
Rachel <rachel.l.pavilanis@gmail.com>
2016-11-18 15:33:20 -0800

    corrected something in model files for posts and projects

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,6 +4,6 @@ class Post < ActiveRecord::Base
   friendly_id :title, use: :slugged
 
   has_attached_file :picture, styles: { large: "600x600>", medium: "300x300>", thumb: "150x150#" }
-  validates_attachment_content_type :picture, content_type: /\Apicture\/.*\z/
+  validates_attachment_content_type :picture, content_type: /\Aimage\/.*\z/
 
 end
