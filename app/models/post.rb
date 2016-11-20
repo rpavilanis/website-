73a6e2135d5e31252966e6270c98142d2f95a0c5
Rachel <rachel.l.pavilanis@gmail.com>
2016-11-20 13:04:23 -0800

    continue blog formatting

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,7 +3,7 @@ class Post < ActiveRecord::Base
   extend FriendlyId
   friendly_id :title, use: :slugged
 
-  has_attached_file :picture, styles: { large: "600x600>", medium: "300x300>", thumb: "150x150#" }
+  has_attached_file :picture, styles: { large: "700x700>", medium: "300x300>", thumb: "150x150#" }
   validates_attachment_content_type :picture, content_type: /\Aimage\/.*\z/
 
 end
