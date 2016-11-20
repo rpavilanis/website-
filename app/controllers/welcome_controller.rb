766c71a30c80c63c723dd54358069f7f7dda557f
Rachel <rachel.l.pavilanis@gmail.com>
2016-11-19 18:49:13 -0800

    add an about page.

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -4,4 +4,17 @@ class WelcomeController < ApplicationController
 
     @projects = Project.all.limit(3).order("created_at desc")
   end
+
+  def show
+      if valid_page?
+        render template: "welcome/#{params[:page]}"
+      else
+        render file: "public/404.html", status: :not_found
+      end
+  end
+
+  private
+  def valid_page?
+    File.exist?(Pathname.new(Rails.root + "app/views/welcome/#{params[:page]}.html.erb"))
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'welcome#index'
 
+  get "/welcome/:page" => "pages#show"
+
   resources :posts
   resources :projects
 
